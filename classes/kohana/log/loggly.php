e4948b93024c6cac9ec6da52cbbe305786e4a37b
Simon Wood <simon.wood@holidayextras.com>
2012-01-06 00:01:17 +0000

    Initial check in. Classes fleshed out.

---
+++ classes/kohana/log/loggly.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Kohana Loggly is a Log Writer that sends all log messages to a loggly account.
+ *
+ * @author	Simon Wood	<hpoomdev@gmail.com>
+ */
+class Log_Loggly extends Log_Writer
+{
+
+}
---
+++ classes/log/loggly.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Log_Loggly extends Kohana_Log_Loggly {}
