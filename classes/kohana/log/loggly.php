f729fb9ec555fca1bfe2f79237f8d418b13c8a1e
Simon Wood <simon.wood@holidayextras.com>
2012-01-06 10:36:49 +0000

    Initial Commit

--- classes/kohana/log/loggly.php
+++ classes/kohana/log/loggly.php
@@ -5,7 +5,53 @@
  *
  * @author	Simon Wood	<hpoomdev@gmail.com>
  */
-class Log_Loggly extends Log_Writer
-{
+class Kohana_Log_Loggly extends Log_Writer {
+	/**
+	 * @var  string  Input key used for logging
+	 */
+	protected $_inputKey;
+	
+	/**
+	 * Creates a new loggly logger. Checks that the key is in the correct format and length.
+	 *
+	 *     $writer = new Log_File( $inputKey );
+	 *
+	 * @param   string  loggly input key
+	 * @return  void
+	 */
+	public function __construct( $inputKey ) {
+		// Check key is valid and store it for use in the write method
+		if ( !is_string( $inputKey ) || 36 !== strlen( $inputKey ) ) {
+			throw new Kohana_Exception( 'Loggly key :key must be a valid 36 character string',
+				array( ':key' => $inputKey ) );
+		}
 
+		$this->_inputKey = $inputKey;
+	}
+	
+	/**
+	 * Generates the log message string and posts it to loggly.
+	 *
+	 * @param array $messages
+	 * @return void
+	 */
+	public function write( array $messages ){
+
+		$logString = '';
+		
+		foreach ( $messages as $message ) {
+			foreach ( $message as $title => $body ) {
+				if ( $title === 'level' ) {
+					$body = $this->_log_levels[$body];
+					$logString .= $body.'::';
+				} else if ( $title === 'body' ) {
+					$logString .= $body;
+				}
+			}
+		}
+		
+		$request = Request::factory( 'https://logs.loggly.com/inputs/' . $this->_inputKey )->headers( 'content-type', 'test/plain' )->body( $logString );
+		$response = $request->execute();
+	}
 }
+	
--- classes/log/loggly.php
+++ classes/log/loggly.php
@@ -1,3 +1,3 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-abstract class Log_Loggly extends Kohana_Log_Loggly {}
+class Log_Loggly extends Kohana_Log_Loggly {}
